45d16855df0775d41431eff6bc1959f2e0b679f8
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2026-02-01 00:08:06 -0300

    Implementing Architecture

---
+++ eTasks-server.Core/BusinessLogicLayers/VersionBLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+{
+    
+}
+
+namespace eTasks_server.Core.BusinessLayers
+{
+    public class VersionBLL
+    {
+        public static Results GetVersion()
+        {
+            var version = Models.Version.GetCurrentVersion();
+            return Results.Ok(new
+            {
+                version.AppVersion,
+                version.DisplayVersion,
+                version.URL_APK,
+                version.URL_Win
+            });
+        }
+    }
+}
---
+++ eTasks-server.Core/Models/Version.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eTasks_server.Core.Models
+{
+    public class Version
+    {
+        public int AppVersion { get; set; } = 1;
+        public string DisplayVersion { get; set; } = "2.0.0";
+        public string URL_APK { get; set; } = "https://github.com/rafael-figueiredo-alves/eTasks/releases/download/v1.1/eTasks.apk";
+        public string URL_Win { get; set; } = "https://github.com/rafael-figueiredo-alves/eTasks/releases/download/v1.1/eTasks.exe";
+
+        public static Version GetCurrentVersion()
+        {
+            return new Version();
+        }
+    }
+}
