03f41f6db62d041acceaf6a3a8264477262102af
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2026-02-26 23:12:46 -0300

    Started to work on Error handling

--- eTasks-server/eTasks-server/Program.cs
+++ eTasks-server/eTasks-server/Program.cs
@@ -4,8 +4,10 @@ using eTasks_server.Client.Services.Interfaces;
 using eTasks_server.Components;
 using eTasks_server.Core.Data;
 using eTasks_server.Endpoints;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
+using MySqlConnector;
 using System.Buffers.Text;
 using System.Reflection.Metadata.Ecma335;
 
@@ -56,6 +58,36 @@ builder.Services.AddScoped<IVersionService, VersionService>();
 var app = builder.Build();
 
 app.UseCors("WASMAppPolicy");
+
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+        var exception = exceptionHandlerPathFeature?.Error;
+
+        context.Response.ContentType = "application/json";
+
+        if (exception is MySqlException)
+        {
+            context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+
+            await context.Response.WriteAsJsonAsync(new
+            {
+                message = "Banco de dados indisponível. Tente novamente mais tarde."
+            });
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            await context.Response.WriteAsJsonAsync(new
+            {
+                message = "Erro interno inesperado."
+            });
+        }
+    });
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
